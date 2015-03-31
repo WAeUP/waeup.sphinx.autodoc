915e5d6ce0776f4bc32c72c75994fa70cf5d724c
ulif <uli@gnufix.de>
2015-03-31 08:52:56 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,69 @@
+import os
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+tests_path = os.path.join(os.path.dirname(__file__), 'tests')
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        args = sys.argv[sys.argv.index('test')+1:]
+        self.test_args = args
+        self.test_suite = True
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+tests_require = [
+    'pytest >= 2.0.3',
+    'pytest-xdist',
+    'pytest-cov',
+    ]
+
+docs_require = [
+    'Sphinx',
+    ]
+
+setup(
+    name="waeup.sphinx.autodoc",
+    version="0.1.dev0",
+    author="Uli Fouquet",
+    author_email="uli@gnufix.de",
+    description=(
+        "Replace sphinx.ext.autodoc and do not stumble over grok."),
+    license="GPL 3.0",
+    keywords="sphinx autodoc grok zope",
+    url="http://pypi.python.org/pypi/waeup.sphinx.autodoc",
+    packages=['waeup'],
+    namespace_packages=['waeup', ],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[],
+    tests_require=tests_require,
+    extras_require=dict(
+        tests=tests_require,
+        docs=docs_require,
+        ),
+    cmdclass={'test': PyTest},
+    entry_points={
+        }
+)
