b7cf0da4fff1de57ba851d51178db7077c3065b8
ulif <uli@gnufix.de>
2015-04-20 12:51:10 +0200

    Start SphinxAppFactory.

--- tests/test_autodoc.py
+++ tests/test_autodoc.py
@@ -1,13 +1,24 @@
 # tests for waeup.sphinxext.autodoc
 import pytest
+from sphinx.application import Sphinx
+
+
+class SphinxAppFactory(object):
+    """A factory providing `Sphinx` apps suitable for testing.
+    """
+    app = None
+
+    def __init__(self):
+        pass
 
 
 @pytest.fixture(scope="function")
 def sphinx_app(request):
-    return None
+    return SphinxAppFactory()
 
 
 class TestAutodoc(object):
 
-    def test_foo(self):
+    def test_foo(self, sphinx_app):
         assert 1 == 1
+        assert getattr(sphinx_app, 'app', 42) != 42
