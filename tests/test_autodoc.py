242fc2f00755b1c23463b2e4c3da0a30e3fd388b
ulif <uli@gnufix.de>
2015-04-20 12:57:48 +0200

    Use tempdir in tests.

--- tests/test_autodoc.py
+++ tests/test_autodoc.py
@@ -1,5 +1,8 @@
 # tests for waeup.sphinxext.autodoc
+import os
 import pytest
+import tempfile
+import shutil
 from sphinx.application import Sphinx
 
 
@@ -9,12 +12,19 @@ class SphinxAppFactory(object):
     app = None
 
     def __init__(self):
+        self.root_dir = tempfile.mkdtemp()
         pass
+
+    def cleanup(self):
+        if os.path.exists(self.root_dir):
+            shutil.rmtree(self.root_dir)
 
 
 @pytest.fixture(scope="function")
 def sphinx_app(request):
-    return SphinxAppFactory()
+    factory = SphinxAppFactory()
+    request.addfinalizer(factory.cleanup)
+    return factory
 
 
 class TestAutodoc(object):
