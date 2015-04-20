e711ada51ff4482b67eb15676c47e632df0031fc
ulif <uli@gnufix.de>
2015-04-20 13:57:21 +0200

    Create important workdirs.

--- tests/test_autodoc.py
+++ tests/test_autodoc.py
@@ -12,12 +12,21 @@ class SphinxAppFactory(object):
     app = None
 
     def __init__(self):
-        self.root_dir = tempfile.mkdtemp()
-        pass
+        self.work_dir = tempfile.mkdtemp()
+        # copy local 'sample/' to temp-location
+        self.src_dir = os.path.join(self.work_dir, 'sample')
+        local_src = os.path.join(os.path.dirname(__file__), 'sample')
+        shutil.copytree(local_src, self.src_dir)
+        self.build_dir = os.path.join(self.src_dir, '_build')
+        self.conf_dir = self.src_dir
+        self.out_dir = os.path.join(self.build_dir, 'html')
+        self.doctree_dir = os.path.join(self.build_dir, 'doctrees')
+        os.makedirs(self.out_dir)
+        os.makedirs(self.doctree_dir)
 
     def cleanup(self):
-        if os.path.exists(self.root_dir):
-            shutil.rmtree(self.root_dir)
+        if os.path.exists(self.work_dir):
+            shutil.rmtree(self.work_dir)
 
 
 @pytest.fixture(scope="function")
