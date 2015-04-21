92a84d6592844eab969046d75ba052190b88d8c8
ulif <uli@gnufix.de>
2015-04-21 09:01:17 +0200

    Make sample app a more real-life example.

--- tests/sample/sampleapp.py
+++ tests/sample/sampleapp.py
@@ -1,8 +1,39 @@
 """Sample module.
 """
+import grok
+from zope import schema
+from zope.interface import Interface
 
 
-class SampleApp(object):
-    """The SampleApp is here to be documented: SampleApp_docstring
+class ISampleApp(Interface):
+    """An application implementing: ISampleApp_docstring
     """
     pass
+
+
+class ISampleAppItem(Interface):
+    """An item implementing: ISampleAppItem_docstring
+    """
+    name = schema.TextLine(
+        title=u'Name',
+        )
+
+
+class SampleApp(grok.Application, grok.Container):
+    """The SampleApp is here to be documented: SampleApp_docstring
+    """
+    grok.implements(ISampleApp)
+
+
+class SampleAppItem(grok.Model):
+    """A SampleAppItem: SampleAppItem_docstring
+    """
+    grok.implements(ISampleAppItem)
+
+
+class SampleAppCatalog(grok.Indexes):
+    """A catalog of SampleApps installed: SampleAppCatalog_docstring
+    """
+    grok.site(ISampleApp)
+    grok.name('sample app catalog')
+    grok.context(ISampleAppItem)
