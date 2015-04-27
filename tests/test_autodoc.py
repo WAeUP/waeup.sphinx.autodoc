a6a7d9879ce35973a0cb69b3c5c790dc57678391
ulif <uli@gnufix.de>
2015-04-27 23:55:20 +0200

    Remove old fixture.

--- tests/test_autodoc.py
+++ tests/test_autodoc.py
@@ -14,47 +14,6 @@ LOCAL_TEST_DIR = py.path.local(os.path.dirname(__file__) or '.')
 SAMPLE_SPHINX_SRC = LOCAL_TEST_DIR / "sample"
 
 
-class SphinxAppFactory(object):
-    """A factory providing `Sphinx` apps suitable for testing.
-
-    Expects a `py.path` path as temporary dir.
-
-    A `Sphinx` application instance is available as `app`.
-    """
-    app = None
-
-    def __init__(self, work_dir):
-        self.work_dir = work_dir
-        self.src_dir = self.work_dir / 'sample'
-        SAMPLE_SPHINX_SRC.copy(self.src_dir)
-        self.build_dir = self.src_dir.mkdir('_build')
-        self.conf_dir = self.src_dir
-        self.out_dir = self.build_dir.mkdir('html')
-        self.doctree_dir = self.build_dir.mkdir('doctrees')
-        self.app = Sphinx(
-            self.src_dir.strpath, self.conf_dir.strpath,
-            self.out_dir.strpath, self.doctree_dir.strpath,
-            'html', freshenv=True)
-
-    def build(self):
-        """Build the sample HTML docs.
-        """
-        return self.app.build(force_all=True)
-
-    def cleanup(self):
-        self.work_dir.remove(rec=1)
-        return
-
-
-@pytest.fixture(scope="session")
-def sphinx_app(request):
-    tmpdir = py.path.local(tempfile.mkdtemp())
-    factory = SphinxAppFactory(tmpdir)
-    factory.build()
-    request.addfinalizer(factory.cleanup)
-    return factory
-
-
 @pytest.fixture(scope="session")
 def static_sphinx(request):
     """A fixture that provides a static sphinx build.
